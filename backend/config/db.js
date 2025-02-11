e5e37320bff08e8c9c2f63a6082be11859814262
Simandhar <121376696+Simandhar14@users.noreply.github.com>
2025-02-11 14:07:09 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://baidsimandhar:8bpFyktafizQ2FNh@cluster0.h8hzkzq.mongodb.net/food-del"
+      "DATABASE_URL"
     )
     .then(() => console.log("DB Connected"));
 };
